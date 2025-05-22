bb3144787738f37dfdd5c84b0a0b3d1cbc4335e8
BoVlad <minecraft@karnyz.com>
2025-05-22 20:16:27 +0300

    1

---
+++ Lesson 2/1.py
@@ -0,0 +1,18 @@
+import random
+b = random.randint(0, 100)
+
+print("I\nlove\npython")print(10 + 10) #Вам дано список чисел
+
+
+b = 5
+if b > 0:
+    print("Bilshe 0")
+else:
+    print("Menshe 0")
+
+
+for i in range(100):
+    b = random.randint(0, 100)
+    print(b)
+
+
