625bcd360034405e828edc7adc4dd4d2d1461543
BoVlad <minecraft@karnyz.com>
2025-06-23 19:46:55 +0300

    1

---
+++ Module 2/Lesson 6/3.py
@@ -0,0 +1,6 @@
+for i in range(1, 11):
+    print()
+    for j in range(1, 11):
+        print()
+        for k in range(1, 11):
+            print(f"{i} * {j} * {k} = {i * j * k}", end=" -> ")
---
+++ Module 2/Lesson 6/4.py
@@ -0,0 +1,3 @@
+number = int(input("Введіть число: "))
+for i in range(1, 11):
+    print(f"{number} * {i} = {number * i}")
