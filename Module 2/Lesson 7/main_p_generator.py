19d94cb9bc195911169470b7e55eb3db8dd4058f
BoVlad <minecraft@karnyz.com>
2025-06-25 20:53:49 +0300

    1

--- Module 2/Lesson 7/main_p_generator.py
+++ Module 2/Lesson 7/main_p_generator.py
@@ -1,19 +1,39 @@
 import operations_p_generator
 import string
 
-print("------------------------------")
-print("Це генератор пароля. ")
-print("------------------------------")
-print("Генератор цифрового пароля - 1")
-print("Генератор літерного пароля - 2")
-print("------------------------------")
+menu_list = """------------------------------
+Це генератор пароля.
+------------------------------
+Генератор цифрового пароля - 1
+Генератор літерного пароля - 2
+Вийти - 3
+------------------------------"""
 
-menu = int(input("Введіть ваш вибір: "))
-
-if menu == 1:
-    len_of_p = int(input("Введіть довжину пароля: "))
-    print("Ось ваш згенерований пароль: ", end="")
-    operations_p_generator.p_number(len_of_p)
-if menu == 2:
-
-
+while True:
+    print()
+    print()
+    print(menu_list)
+    print()
+    menu = int(input("Введіть ваш вибір: "))
+    if menu == 1:
+        len_of_p = int(input("Введіть довжину пароля: "))
+        operations_p_generator.p_number(len_of_p)
+    if menu == 2:
+        print()
+        print("-----------------------------------------")
+        print("Генератор літерного пароля маленькими - 1")
+        print("Генератор літерного пароля великими - 2")
+        print("Генератор літерного пароля впереміш - 3")
+        print("-----------------------------------------")
+        menu2 = int(input("Введіть ваш вибір: "))
+        if menu2 == 1:
+            len_of_p = int(input("Введіть довжину пароля: "))
+            operations_p_generator.p_letter1(len_of_p)
+        if menu2 == 2:
+            len_of_p = int(input("Введіть довжину пароля: "))
+            operations_p_generator.p_letter2(len_of_p)
+        if menu2 == 3:
+            len_of_p = int(input("Введіть довжину пароля: "))
+            operations_p_generator.p_letter3(len_of_p)
+    if menu == 3:
+        break
--- Module 2/Lesson 7/operations_p_generator.py
+++ Module 2/Lesson 7/operations_p_generator.py
@@ -1,18 +1,53 @@
 import random
+from random import randint
+
+ascii_lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+ascii_uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+ascii_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                 "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+
 def p_number(len_of_p):
+    print("Ось ваш згенерований пароль: ", end="")
     for i in range(len_of_p):
         print(random.randint(0, 10), end="")
-def p_letter(leon)
+    return
 
 
-whitespace = ' \t\n\r\v\f'
-ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
-ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-ascii_letters = ascii_lowercase + ascii_uppercase
-digits = '0123456789'
-hexdigits = digits + 'abcdef' + 'ABCDEF'
-octdigits = '01234567'
-punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
-printable = digits + ascii_letters + punctuation + whitespace
+def p_letter1(len_of_p):
+    print("Ось ваш згенерований пароль: ", end="")
+    for i in range(len_of_p):
+        random.shuffle(ascii_lowercase)
+        random.shuffle(ascii_lowercase)
+        print(f"{ascii_lowercase[3]}", end="")
+    return
+
+def p_letter2(len_of_p):
+    print("Ось ваш згенерований пароль: ", end="")
+    for i in range(len_of_p):
+        random.shuffle(ascii_uppercase)
+        random.shuffle(ascii_uppercase)
+        print(f"{ascii_uppercase[3]}", end="")
+    return
+
+
+def p_letter3(len_of_p):
+    print("Ось ваш згенерований пароль: ", end="")
+    for i in range(len_of_p):
+        random.shuffle(ascii_letters)
+        random.shuffle(ascii_letters)
+        print(f"{ascii_letters[3]}", end="")
+    return
+
+    # whitespace = ' \t\n\r\v\f'
+    # ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
+    # ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    # ascii_letters = ascii_lowercase + ascii_uppercase
+    # digits = '0123456789'
+    # hexdigits = digits + 'abcdef' + 'ABCDEF'
+    # octdigits = '01234567'
+    # punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+    # printable = digits + ascii_letters + punctuation + whitespace
+
 
 
